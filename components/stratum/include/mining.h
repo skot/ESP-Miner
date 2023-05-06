bbd2ebe2492cbdb8b0737f8578d5c4a6382c6070
johnny9 <985648+johnny9@users.noreply.github.com>
2023-05-06 16:39:51 -0400

    mining: fix endianness of the block header version

--- components/stratum/include/mining.h
+++ components/stratum/include/mining.h
@@ -16,7 +16,7 @@ char * construct_coinbase_tx(const char * coinbase_1, const char * coinbase_2,
 
 char * calculate_merkle_root_hash(const char * coinbase_tx, const uint8_t merkle_branches[][32], const int num_merkle_branches);
 
-bm_job construct_bm_job(const char * version, const char * prev_block_hash, const char * merkle_root,
+bm_job construct_bm_job(uint32_t version, const char * prev_block_hash, const char * merkle_root,
                         uint32_t ntime, uint32_t target);
 
 #endif // MINING_H
