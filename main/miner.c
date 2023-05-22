6eafb362a0e076c61f8bbe26ab748ae91a7e69ed
johnny9 <985648+johnny9@users.noreply.github.com>
2023-05-22 17:31:45 -0400

    main: start SysTask before anything else

--- main/miner.c
+++ main/miner.c
@@ -264,13 +264,13 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
+    xTaskCreate(SysTask, "System_Task", 4096, NULL, 10, &sysTaskHandle);
+
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
      */
     ESP_ERROR_CHECK(example_connect());
-
-    xTaskCreate(SysTask, "System_Task", 4096, NULL, 10, &sysTaskHandle);
 
     queue_init(&g_queue);
     queue_init(&g_bm_queue);
