6bb4bc3a131c1885c19943fcd4905326a3dd28e6
johnny9 <985648+johnny9@users.noreply.github.com>
2023-05-25 20:43:52 -0400

    mining: add test to check nonce diff
    Test unconvered issues in the construction of the header
    when hashing as well as issues with converting the hash to
    a double.

--- components/stratum/include/utils.h
+++ components/stratum/include/utils.h
@@ -10,6 +10,7 @@ size_t bin2hex(const uint8_t * buf, size_t buflen, char * hex, size_t hexlen);
 
 uint8_t hex2val(char c);
 void flip80bytes(void *dest_p, const void *src_p);
+void flip32bytes(void *dest_p, const void *src_p);
 
 size_t hex2bin(const char * hex, uint8_t * bin, size_t bin_len);
 
@@ -28,5 +29,7 @@ void swap_endian_words(const char * hex,  uint8_t * output);
 void reverse_bytes(uint8_t * data, size_t len);
 
 double le256todouble(const void *target);
+
+void prettyHex(unsigned char * buf, int len);
 
 #endif // STRATUM_UTILS_H
