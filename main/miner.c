7226f569eb49164e9a16967847584679e5a4bce5
Skot Croshere <skot@geocene.com>
2023-06-12 20:42:49 -0400

    cleanup connect defines

--- main/miner.c
+++ main/miner.c
@@ -38,7 +38,7 @@ void app_main(void)
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
      */
-    ESP_ERROR_CHECK(example_connect());
+    ESP_ERROR_CHECK(wifi_connect());
 
     queue_init(&GLOBAL_STATE.stratum_queue);
     queue_init(&GLOBAL_STATE.ASIC_jobs_queue);
