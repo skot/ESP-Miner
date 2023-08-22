3981782edb572b230a843d770d7b3d8cacd4539e
Ben <wilsob12@gmail.com>
2023-08-21 23:37:12 -0400

    axe os

---
+++ main/http_server/axe-os/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+const components = [
+  AppComponent,
+  HeaderComponent
+];
+
+@NgModule({
+  declarations: [
+    ...components
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
