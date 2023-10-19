bf91212c3e8086c580069c748208601de4303681
tommy <coder@tommywatson.com>
2023-10-19 16:40:33 -0400

    Save "Best Difficulty" scores into NVS #20

--- main/nvs_config.h
+++ main/nvs_config.h
@@ -16,10 +16,13 @@
 #define NVS_CONFIG_BOARD_VERSION "boardversion"
 #define NVS_CONFIG_FLIP_SCREEN "flipscreen"
 #define NVS_CONFIG_INVERT_SCREEN "invertscreen"
+#define NVS_CONFIG_BEST_DIFF "bestdiff"
 
 char * nvs_config_get_string(const char * key, const char * default_value);
 void nvs_config_set_string(const char * key, const char * default_value);
 uint16_t nvs_config_get_u16(const char * key, const uint16_t default_value);
 void nvs_config_set_u16(const char * key, const uint16_t value);
+uint64_t nvs_config_get_u64(const char * key, const uint64_t default_value);
+void nvs_config_set_u64(const char * key, const uint64_t value);
 
 #endif // MAIN_NVS_CONFIG_H
