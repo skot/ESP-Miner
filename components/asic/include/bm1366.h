cfc10a2e0ce9fe8c3aeea02aa48972a09a3e6d17
Robin van der Gracht <robinvandergracht@gmail.com>
2023-10-22 09:26:17 +0200

    asic: bm1366: Improve performance of hash frequency divider calculation
    We should avoid using floating point arithmetic. Floating point
    calculations are always slower than integer calculations, especially
    on platforms without an FPU.
    
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- components/asic/include/bm1366.h
+++ components/asic/include/bm1366.h
@@ -35,7 +35,7 @@ void BM1366_send_work(void * GLOBAL_STATE, bm_job * next_bm_job);
 void BM1366_set_job_difficulty_mask(int);
 int BM1366_set_max_baud(void);
 int BM1366_set_default_baud(void);
-void BM1366_send_hash_frequency(float frequency);
+void BM1366_send_hash_frequency(unsigned int khz);
 task_result * BM1366_proccess_work(void * GLOBAL_STATE);
 
 #endif /* BM1366_H_ */
