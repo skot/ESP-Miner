9e39d516f77444e0c76934fefc96751b19b426a8
Robin van der Gracht <robinvandergracht@gmail.com>
2023-10-31 14:46:46 +0100

    Migrate to the new oneshot API for ADC conversions.
    Cannot compile adc.c since the legacy API is no longer available.
    
    #warning "legacy adc driver is deprecated" please migrate to use
    esp_adc/adc_oneshot.h and esp_adc/adc_continuous.h for oneshot mode and
    continuous mode drivers respectively"
    
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- main/adc.c
+++ main/adc.c
@@ -1,19 +1,38 @@
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
+#include "esp_err.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
 
 // static const char *TAG = "adc.c";
-static esp_adc_cal_characteristics_t adc1_chars;
+static adc_oneshot_unit_handle_t adc1_handle;
+static adc_cali_handle_t adc1_cali_handle;
 
 // Sets up the ADC to read Vcore. Run this before ADC_get_vcore()
 void ADC_init(void)
 {
-    adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_DB_11);
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+	adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_11,
+    };
+    adc_cali_curve_fitting_config_t cali_config = {
+        .unit_id = ADC_UNIT_1,
+        .atten = ADC_ATTEN_DB_11,
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_1, &config));
+    ESP_ERROR_CHECK(adc_cali_create_scheme_curve_fitting(&cali_config, &adc1_cali_handle));
 }
 
 // returns the ADC voltage in mV
+// Avoid calling from multiple threads without locks
 uint16_t ADC_get_vcore(void)
 {
-    adc1_config_width(ADC_WIDTH_BIT_DEFAULT);
-    return esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_1), &adc1_chars);
+    int adc_raw_voltage, adc_voltage;
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_1, &adc_raw_voltage));
+    ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw_voltage, &adc_voltage));
+    return adc_voltage;
 }
