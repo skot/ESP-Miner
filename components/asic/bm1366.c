f1735bb45640135f120b2ac121d2f692e2e5fdeb
Robin van der Gracht <robinvandergracht@gmail.com>
2024-01-20 23:09:38 +0100

    asic: bm1366: Use sizeof to determine size of asic_response_buffer
    This way we only have the buffer size defined in one location.
    
    Signed-off-by: Robin van der Gracht <robinvandergracht@gmail.com>

--- components/asic/bm1366.c
+++ components/asic/bm1366.c
@@ -344,7 +344,7 @@ uint8_t BM1366_init(uint64_t frequency, uint16_t asic_count)
 {
     ESP_LOGI(TAG, "Initializing BM1366");
 
-    memset(asic_response_buffer, 0, 1024);
+    memset(asic_response_buffer, 0, sizeof(asic_response_buffer));
 
     esp_rom_gpio_pad_select_gpio(BM1366_RST_PIN);
     gpio_set_direction(BM1366_RST_PIN, GPIO_MODE_OUTPUT);
