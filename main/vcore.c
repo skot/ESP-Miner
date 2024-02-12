bd1bd73d0e678bb04f58b83ee86a9ffdf75fe9f5
macphyter <macphyter@gmail.com>
2024-02-11 21:31:29 -0700

    Read vcore properly in GUI

--- main/vcore.c
+++ main/vcore.c
@@ -76,5 +76,8 @@ bool VCORE_set_voltage(float core_voltage, GlobalState * global_state)
 }
 
 uint16_t VCORE_get_voltage_mv(GlobalState * global_state) {
+    if (global_state->device_model == DEVICE_HEX) {
+        return (TPS546_get_vout() * 1000) / global_state->voltage_domain;
+    }
     return ADC_get_vcore() / global_state->voltage_domain;
 }
