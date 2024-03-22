252f936fb8e5621c44f69b379009d0fd0de8d0bf
Saro G. <sarog@users.noreply.github.com>
2024-03-22 09:07:39 -0400

    Replaced u_int64_t with uint64_t

--- components/bm1397/include/bm1368.h
+++ components/bm1397/include/bm1368.h
@@ -7,11 +7,11 @@
 
 #define CRC5_MASK 0x1F
 
-// static const u_int64_t BM1368_FREQUENCY = CONFIG_ASIC_FREQUENCY;
-static const u_int64_t BM1368_CORE_COUNT = 672;
-// static const u_int64_t BM1368_HASHRATE_S = BM1368_FREQUENCY * BM1368_CORE_COUNT * 1000000;
+// static const uint64_t BM1368_FREQUENCY = CONFIG_ASIC_FREQUENCY;
+static const uint64_t BM1368_CORE_COUNT = 672;
+// static const uint64_t BM1368_HASHRATE_S = BM1368_FREQUENCY * BM1368_CORE_COUNT * 1000000;
 //  2^32
-//  static const u_int64_t NONCE_SPACE = 4294967296;
+//  static const uint64_t NONCE_SPACE = 4294967296;
 static const double BM1368_FULLSCAN_MS = 2140;
 
 typedef struct
@@ -31,7 +31,7 @@ typedef struct __attribute__((__packed__))
     uint8_t version[4];
 } BM1368_job;
 
-uint8_t BM1368_init(u_int64_t frequency);
+uint8_t BM1368_init(uint64_t frequency);
 
 uint8_t BM1368_send_init(void);
 void BM1368_send_work(void * GLOBAL_STATE, bm_job * next_bm_job);
--- main/global_state.h
+++ main/global_state.h
@@ -16,7 +16,7 @@
 
 typedef struct
 {
-    uint8_t (*init_fn)(u_int64_t);
+    uint8_t (*init_fn)(uint64_t);
     task_result * (*receive_result_fn)(void * GLOBAL_STATE);
     int (*set_max_baud_fn)(void);
     void (*set_difficulty_mask_fn)(int);
