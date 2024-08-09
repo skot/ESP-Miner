3be83cd1b1036d773065c11dea79df202eb47164
onlyblackstars <74259365+onlyblackstars@users.noreply.github.com>
2024-08-09 16:07:22 -0400

    minor change to ckpool quicklink detector

--- main/http_server/axe-os/src/app/components/home/home.component.ts
+++ main/http_server/axe-os/src/app/components/home/home.component.ts
@@ -147,7 +147,7 @@ export class HomeComponent {
         } else if (info.stratumURL.includes('solo.d-central.tech')) {
           const address = info.stratumUser.split('.')[0]
           return `https://solo.d-central.tech/#/app/${address}`;
-        } else if (info.stratumURL.includes('solo.ckpool.org')) {
+        } else if (/solo[46]?.ckpool.org/.test(info.stratumURL)) {
           const address = info.stratumUser.split('.')[0]
           return `https://solostats.ckpool.org/stats/${address}`;
         } else {
