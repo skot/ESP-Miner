31c96ac7ae4b0b0ad53b64a8817f37b1b329710c
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2024-08-11 19:27:32 +0200

    Fix precision for devices reaching TH/s

--- main/http_server/axe-os/src/app/pipes/hash-suffix.pipe.ts
+++ main/http_server/axe-os/src/app/pipes/hash-suffix.pipe.ts
@@ -26,7 +26,7 @@ export class HashSuffixPipe implements PipeTransform {
     const scaledValue = value / Math.pow(1000, power);
     const suffix = suffixes[power];
 
-    return scaledValue.toFixed(1) + suffix;
+    return scaledValue.toFixed(2) + suffix;
   }
 
 
