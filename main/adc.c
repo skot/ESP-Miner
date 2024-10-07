6ac75822bfa8c13179e7950550d2c97a72a7cce1
Skot <skot@bitnet.cx>
2024-10-07 12:03:36 -0400

    fix some misc function paramater type warnings

--- main/adc.c
+++ main/adc.c
@@ -7,8 +7,8 @@ static esp_adc_cal_characteristics_t adc1_chars;
 // Sets up the ADC to read Vcore. Run this before ADC_get_vcore()
 void ADC_init(void)
 {
-    adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_DB_11);
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+    adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_DB_12);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
 }
 
 // returns the ADC voltage in mV
