7768004b680fa9939387f9c2c096ff1194b51602
mutatrum <mutatrum@gmail.com>
2024-12-02 11:28:36 +0100

    Don't divide by zero on boot

--- main/screen.c
+++ main/screen.c
@@ -259,7 +259,10 @@ static void screen_update_cb(lv_timer_t * timer)
     }
 
     if (current_power != power_management->power || current_hashrate != module->current_hashrate) {
-        lv_label_set_text_fmt(efficiency_label, "J/Th: %.2f", power_management->power / (module->current_hashrate / 1000.0));
+        if (power_management->power > 0 && module->current_hashrate > 0) {
+            float efficiency = power_management->power / (module->current_hashrate / 1000.0);
+            lv_label_set_text_fmt(efficiency_label, "J/Th: %.2f", efficiency);
+        }
     }
 
     if (current_difficulty != module->best_session_nonce_diff) {
