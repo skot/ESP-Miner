6283480fa1111fa5a07b775db1d50db51059f584
mutatrum <mutatrum@gmail.com>
2024-12-09 16:35:13 +0100

    Parse stratum api reject reason (#472)
    * Parse stratum api reject reason
    
    * Remove superfluous nullcheck

--- main/tasks/stratum_task.c
+++ main/tasks/stratum_task.c
@@ -302,14 +302,14 @@ void stratum_task(void * pvParameters)
                     ESP_LOGI(TAG, "message result accepted");
                     SYSTEM_notify_accepted_share(GLOBAL_STATE);
                 } else {
-                    ESP_LOGW(TAG, "message result rejected");
+                    ESP_LOGW(TAG, "message result rejected: %s", stratum_api_v1_message.error_str ? stratum_api_v1_message.error_str : "unknown");
                     SYSTEM_notify_rejected_share(GLOBAL_STATE);
                 }
             } else if (stratum_api_v1_message.method == STRATUM_RESULT_SETUP) {
                 if (stratum_api_v1_message.response_success) {
                     ESP_LOGI(TAG, "setup message accepted");
                 } else {
-                    ESP_LOGE(TAG, "setup message rejected");
+                    ESP_LOGE(TAG, "setup message rejected: %s", stratum_api_v1_message.error_str ? stratum_api_v1_message.error_str : "unknown");
                 }
             }
         }
