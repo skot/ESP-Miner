9a06817f850a3550c8d2d0c4d031d64c9efa586e
adam west <adammwest1@gmail.com>
2025-02-10 13:19:03 +0100

    update funcs

--- components/asic/include/common.h
+++ components/asic/include/common.h
@@ -13,7 +13,7 @@ typedef struct __attribute__((__packed__))
 unsigned char _reverse_bits(unsigned char num);
 int _largest_power_of_two(int num);
 int calulate_hcn_from_nonce_percent(int chip_addr,int freq,float nonce_percent);
-float calculate_timeout_ms(int chip_addr,int freq, int cno, float nonce_percent, float timeout_percent,int versions_per_core);
+float calculate_timeout_ms(int big_cores,int chip_addr,int freq, int cno, float nonce_percent, float timeout_percent,int versions_per_core);
 float calculate_cno(int chips) ;
 int calulate_hcn_max(int chip_addr,int freq);
 
