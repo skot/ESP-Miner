cff07856bf21fa7218eee00dd3f2206fdba27151
Erik Olof Gunnar Andersson <me@eandersson.net>
2025-02-21 19:32:49 +0100

    Revert debug changes

--- components/asic/include/bm1366.h
+++ components/asic/include/bm1366.h
@@ -10,10 +10,10 @@
 #define CRC5_MASK 0x1F
 #define BM1366_ASIC_DIFFICULTY 256
 
-#define BM1366_SERIALTX_DEBUG true
-#define BM1366_SERIALRX_DEBUG true
+#define BM1366_SERIALTX_DEBUG false
+#define BM1366_SERIALRX_DEBUG false
 #define BM1366_DEBUG_WORK false //causes insane amount of debug output
-#define BM1366_DEBUG_JOBS true //causes insane amount of debug output
+#define BM1366_DEBUG_JOBS false //causes insane amount of debug output
 
 static const uint64_t BM1366_CORE_COUNT = 112;
 static const uint64_t BM1366_SMALL_CORE_COUNT = 894;
