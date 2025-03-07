2cce8981615f0302f961548fb4fa8400527a84d4
WantClue <Lars_Naether@hotmail.de>
2025-03-07 20:25:10 +0100

    refactor asic model code to asic.c

--- main/tasks/power_management_task.c
+++ main/tasks/power_management_task.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include "INA260.h"
-#include "bm1397.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,6 +12,7 @@
 #include "vcore.h"
 #include "thermal.h"
 #include "power.h"
+#include "asic.h"
 
 #define POLL_RATE 2000
 #define MAX_TEMP 90.0
@@ -143,36 +143,13 @@ void POWER_MANAGEMENT_task(void * pvParameters)
 
         if (asic_frequency != last_asic_frequency) {
             ESP_LOGI(TAG, "New ASIC frequency requested: %uMHz (current: %uMHz)", asic_frequency, last_asic_frequency);
-            bool success = false;
             
-            // Call the appropriate set_frequency function based on the ASIC model
-            switch (GLOBAL_STATE->asic_model) {
-                case ASIC_BM1366:
-                    success = BM1366_set_frequency((float)asic_frequency);
-                    break;
-                case ASIC_BM1368:
-                    success = BM1368_set_frequency((float)asic_frequency);
-                    break;
-                case ASIC_BM1370:
-                    success = BM1370_set_frequency((float)asic_frequency);
-                    break;
-                case ASIC_BM1397:
-                    // BM1397 doesn't have a set_frequency function yet
-                    ESP_LOGE(TAG, "Frequency transition not implemented for BM1397");
-                    success = false;
-                    break;
-                default:
-                    ESP_LOGE(TAG, "Unknown ASIC model, cannot set frequency");
-                    success = false;
-                    break;
-            }
+            bool success = ASIC_set_frequency(GLOBAL_STATE, (float)asic_frequency);
             
             if (success) {
                 power_management->frequency_value = (float)asic_frequency;
-                ESP_LOGI(TAG, "Successfully transitioned to new ASIC frequency: %uMHz", asic_frequency);
-            } else {
-                ESP_LOGE(TAG, "Failed to transition to new ASIC frequency: %uMHz", asic_frequency);
             }
+            
             last_asic_frequency = asic_frequency;
         }
 
