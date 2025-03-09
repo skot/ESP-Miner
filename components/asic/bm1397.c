1eeb303672c876939d53328fd8395e3813a97dcf
Erik Olof Gunnar Andersson <eandersson@users.noreply.github.com>
2025-03-09 13:55:58 +0100

    Refactor Stratum code for Seamless Failover (#717)
    * Refactor stratum code
    
    * No need to abandon work when switching connection
    
    * Bump primary task priority slightly
    
    * Reduce watchdog stack size
    
    * Revert debug changes
    
    * Removed unused code
    
    * Removed unused unit test code
    
    * Handle jobs more aggressively
    
    * Fix merge conflict

--- components/asic/bm1397.c
+++ components/asic/bm1397.c
@@ -360,7 +360,7 @@ void BM1397_set_job_difficulty_mask(int difficulty)
 
 static uint8_t id = 0;
 
-void BM1397_send_work(void *pvParameters, bm_job *next_bm_job)
+int BM1397_send_work(void *pvParameters, bm_job *next_bm_job)
 {
 
     GlobalState *GLOBAL_STATE = (GlobalState *)pvParameters;
@@ -386,6 +386,10 @@ void BM1397_send_work(void *pvParameters, bm_job *next_bm_job)
         memcpy(job.midstate3, next_bm_job->midstate3, 32);
     }
 
+    #if BM1397_DEBUG_JOBS
+    ESP_LOGI(TAG, "Send Job: %02X (%d)", job.job_id, next_bm_job->connection_id);
+    #endif
+
     if (GLOBAL_STATE->ASIC_TASK_MODULE.active_jobs[job.job_id] != NULL)
     {
         free_bm_job(GLOBAL_STATE->ASIC_TASK_MODULE.active_jobs[job.job_id]);
@@ -393,15 +397,9 @@ void BM1397_send_work(void *pvParameters, bm_job *next_bm_job)
 
     GLOBAL_STATE->ASIC_TASK_MODULE.active_jobs[job.job_id] = next_bm_job;
 
-    pthread_mutex_lock(&GLOBAL_STATE->valid_jobs_lock);
-    GLOBAL_STATE->valid_jobs[job.job_id] = 1;
-    pthread_mutex_unlock(&GLOBAL_STATE->valid_jobs_lock);
-
-    #if BM1397_DEBUG_JOBS
-    ESP_LOGI(TAG, "Send Job: %02X", job.job_id);
-    #endif
-
     _send_BM1397((TYPE_JOB | GROUP_SINGLE | CMD_WRITE), (uint8_t *)&job, sizeof(job_packet), BM1397_DEBUG_WORK);
+
+    return job.job_id;
 }
 
 asic_result *BM1397_receive_work(void)
@@ -456,11 +454,6 @@ task_result *BM1397_proccess_work(void *pvParameters)
     uint8_t rx_midstate_index = asic_result->job_id & 0x03;
 
     GlobalState *GLOBAL_STATE = (GlobalState *)pvParameters;
-    if (GLOBAL_STATE->valid_jobs[rx_job_id] == 0)
-    {
-        ESP_LOGW(TAG, "Invalid job nonce found, id=%d", rx_job_id);
-        return NULL;
-    }
 
     uint32_t rolled_version = GLOBAL_STATE->ASIC_TASK_MODULE.active_jobs[rx_job_id]->version;
     for (int i = 0; i < rx_midstate_index; i++)
